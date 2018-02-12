c78383f25b8451b2d56d6c6b73a8dcbcce49b47f
Meng-Gen Tsai <plover@gmail.com>
2018-02-12 23:52:16 +0800

    Setup cache

--- stock-api/api.py
+++ stock-api/api.py
@@ -1,13 +1,20 @@
 from flask import Flask
+from flask_caching import Cache
+
 from analyzers import DupontAnalyzer
 
 
 app = Flask(__name__)
+cache = Cache(app, config={'CACHE_TYPE': 'simple'})
+
 
 @app.route("/v1/stock/analysis/<stock>")
+@cache.cached(timeout=60)
 def analysis(stock):
     return "{stock}".format(stock=stock)
 
+
 @app.route("/v1/stock/help")
+@cache.cached(timeout=1800)
 def help():
     return "help"
