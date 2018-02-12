78f7cb92cbb07fd1344869121d3598333c19bc8e
Meng-Gen Tsai <plover@gmail.com>
2018-02-12 23:17:37 +0800

    Format

--- stock-api/api.py
+++ stock-api/api.py
@@ -3,12 +3,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
-
 @app.route("/v1/stock/analyzer/<analyzer>/<date_frame>/<stock>")
 def analyzer(analyzer, date_frame, stock):
     return "/{analyzer}/{date_frame}/{stock}" \
         .format(analyzer=analyzer, date_frame=date_frame, stock=stock)
-
 
 @app.route("/v1/stock/help")
 def help():
